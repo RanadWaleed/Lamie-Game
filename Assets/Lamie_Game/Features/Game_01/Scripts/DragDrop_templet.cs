e7d8b9fbf19dcc2c146f91d9c4e152eea30a0209
RanadWaleed <rnadaljhdly88@gmail.com>
2026-04-11 01:14:00 +0300

    backend integration for psychometric reports and fix stage 6 logic

--- Assets/Lamie_Game/Features/Game_01/Scripts/DragDrop_templet.cs
+++ Assets/Lamie_Game/Features/Game_01/Scripts/DragDrop_templet.cs
@@ -4,20 +4,20 @@ using UnityEngine.EventSystems;
 public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public RectTransform[] allMolds;
-    public float snapDistance = 150f;
+    public float snapDistance = 100f;
     public float scaleMultiplier = 1.15f;
 
     public string candyID;
     public string selectedMoldName = "";
     public bool isPlaced = false;
 
+    private bool hasFailed = false;
+
     private RectTransform rectTransform;
     private Canvas canvas;
     private CanvasGroup canvasGroup;
-    private Vector2 startPosition;
+    private Vector3 startWorldPosition;
     private Vector3 originalScale;
-
-    private Game2Manager game2Manager;
 
     void Start()
     {
@@ -27,28 +27,30 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         if (canvasGroup == null) canvasGroup = gameObject.AddComponent<CanvasGroup>();
 
         originalScale = transform.localScale;
-        startPosition = rectTransform.anchoredPosition;
-
-        game2Manager = FindObjectOfType<Game2Manager>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        if (isPlaced) return;
+
         canvasGroup.blocksRaycasts = false;
         transform.SetAsLastSibling();
 
+        startWorldPosition = rectTransform.position;
         selectedMoldName = "";
-        isPlaced = false;
         transform.localScale = originalScale;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (isPlaced) return;
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (isPlaced) return;
+
         canvasGroup.blocksRaycasts = true;
 
         RectTransform closestMold = null;
@@ -56,7 +58,7 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
         foreach (RectTransform mold in allMolds)
         {
-            float distance = Vector2.Distance(rectTransform.anchoredPosition, mold.anchoredPosition);
+            float distance = Vector2.Distance(rectTransform.position, mold.position);
             if (distance < minDistance)
             {
                 minDistance = distance;
@@ -64,38 +66,44 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
             }
         }
 
-        if (minDistance <= snapDistance && closestMold != null)
+        if (closestMold != null && minDistance <= snapDistance)
         {
-            bool isOccupied = false;
-            DragDrop[] allCandies = FindObjectsOfType<DragDrop>();
-
-            foreach (DragDrop candy in allCandies)
+            if (closestMold.name == candyID)
             {
-                if (candy != this && candy.selectedMoldName == closestMold.name)
-                {
-                    isOccupied = true;
-                    break;
-                }
-            }
-
-            if (!isOccupied)
-            {
-                rectTransform.anchoredPosition = closestMold.anchoredPosition;
+                rectTransform.position = closestMold.position;
                 transform.localScale = originalScale * scaleMultiplier;
                 selectedMoldName = closestMold.name;
                 isPlaced = true;
 
-                if (game2Manager != null)
+                canvasGroup.blocksRaycasts = false;
+
+                if (MasterManager.Instance != null)
                 {
-                    game2Manager.CheckAllCandiesPlaced();
+                    MasterManager.Instance.RegisterAttempt(true, !hasFailed);
+                }
+
+                if (LevelSpawner.Instance != null && transform.parent.name == "AllCandiesGroup")
+                {
+                    LevelSpawner.Instance.CandyPlacedCorrectly();
+                }
+                else if (Game2Spawner.Instance != null && transform.parent.name == "AllShapesGroup")
+                {
+                    Game2Spawner.Instance.ShapePlacedCorrectly();
                 }
                 return;
             }
         }
 
-        rectTransform.anchoredPosition = startPosition;
+        rectTransform.position = startWorldPosition;
         transform.localScale = originalScale;
         selectedMoldName = "";
         isPlaced = false;
+
+        hasFailed = true;
+
+        if (MasterManager.Instance != null)
+        {
+            MasterManager.Instance.RegisterAttempt(false, false);
+        }
     }
 }
